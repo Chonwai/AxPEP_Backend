b1822028038357d97f09d8a3d45769d3ade57e41
Chonwai Un <chonwaiun@outlook.com>
2020-10-30 16:22:24 +0800

    Add: Added the create_tasks_table migration.

---
+++ database/migrations/2020_10_30_052806_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid('id')->primary()->index();
+            $table->string('email', 100)->index();
+            $table->string('action')->index();
+            $table->string('source', 20)->index();
+            $table->string('description')->nullable();
+            $table->ipAddress('ip');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
