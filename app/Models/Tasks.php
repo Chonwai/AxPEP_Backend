5913343fe3c43aced5914cad9fd2e46b57bd4ef4
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-11-16 15:11:27 +0800

    Add: Added the Task Model.

---
+++ app/Models/Tasks.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tasks extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'email ',
+        'action',
+        'source',
+        'description',
+        'ip',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'ip',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * The attributes that should be setting the default value.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'action' => 'ready',
+    ];
+}
