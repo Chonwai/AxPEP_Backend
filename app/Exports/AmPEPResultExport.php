3bae8750f6a70163098d6f11a86c90bc280c8597
Chonwai <chonwaiun@gmail.com>
2020-11-26 02:36:45 +0800

    Add: Added the AmPEPResultExport.php and export function.

--- app/Exports/AmPEPResultExport.php
+++ app/Exports/AmPEPResultExport.php
@@ -3,8 +3,9 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class AmPEPResultExport implements FromArray
+class AmPEPResultExport implements FromArray, WithHeadings
 {
     protected $data;
 
@@ -16,5 +17,10 @@ class AmPEPResultExport implements FromArray
     public function array(): array
     {
         return $this->data;
+    }
+
+    public function headings(): array
+    {
+        return array_keys($this->data[0]);
     }
 }
--- app/Utils/FileUtils.php
+++ app/Utils/FileUtils.php
@@ -2,6 +2,7 @@
 
 namespace App\Utils;
 
+use App\Exports\AmPEPResultExport;
 use App\Imports\AmPEPResultImport;
 use App\Imports\OutImport;
 use Illuminate\Support\Facades\Storage;
@@ -35,7 +36,6 @@ class FileUtils
             if ($i % 2 == 1) {
                 $id = ltrim($line, ">");
                 $id = ltrim(str_replace("\r\n", '', $id));
-                echo(json_encode($id));
             } else {
                 $sequence = $line;
                 $sequence = ltrim(str_replace("\r\n", '', $sequence));
@@ -47,29 +47,39 @@ class FileUtils
 
     public static function writeResultFile($id, $methods)
     {
+        $classifications = Excel::toArray(new AmPEPResultImport, "Tasks/$id/classification.csv", null, \Maatwebsite\Excel\Excel::CSV);
+        $scores = Excel::toArray(new AmPEPResultImport, "Tasks/$id/score.csv", null, \Maatwebsite\Excel\Excel::CSV);
+
         foreach ($methods as $key => $value) {
-            self::matching($id, $value->method);
+            $matchResult = self::matching($id, $value->method, $classifications, $scores);
+            $classifications = $matchResult[0];
+            $scores = $matchResult[1];
         }
+
+
+
+        Excel::store(new AmPEPResultExport($classifications[0]), "Tasks/$id/classification.csv", null, \Maatwebsite\Excel\Excel::CSV);
+        Excel::store(new AmPEPResultExport($scores[0]), "Tasks/$id/score.csv", null, \Maatwebsite\Excel\Excel::CSV);
     }
 
-    public static function matching($id, $method) {
+    public static function matching($id, $method, $classifications, $scores)
+    {
         $fResult = Excel::toArray(new OutImport, "Tasks/$id/$method.out", null, \Maatwebsite\Excel\Excel::TSV);
-        $classificationArray = Excel::toArray(new AmPEPResultImport, "Tasks/$id/classification.csv", null, \Maatwebsite\Excel\Excel::CSV);
-        $scoreArray = Excel::toArray(new AmPEPResultImport, "Tasks/$id/score.csv", null, \Maatwebsite\Excel\Excel::CSV);
         foreach ($fResult[0] as $key => $value) {
             $result = explode(" ", $value[0]);
-            $classificationArray[0] = array_map(function($value) use ($result, $method) {
+            $classifications[0] = array_map(function ($value) use ($result, $method) {
                 if ($value['id'] == $result[0]) {
                     $value["$method"] = $result[1];
                 }
                 return $value;
-            }, $classificationArray[0]);
-            $scoreArray[0] = array_map(function($value) use ($result, $method) {
+            }, $classifications[0]);
+            $scores[0] = array_map(function ($value) use ($result, $method) {
                 if ($value['id'] == $result[0]) {
-                    $value["$method"] = $result[1];
+                    $value["$method"] = $result[2];
                 }
                 return $value;
-            }, $scoreArray[0]);
+            }, $scores[0]);
         }
+        return [$classifications, $scores];
     }
 }
