e0a8fe3a57aa605f2a9274e9706df97921503a24
Chonwai <chonwaiun@gmail.com>
2022-11-08 03:00:00 +0800

    Update: Updated the CORS value.

--- app/Http/Controllers/Apis/CodonController.php
+++ app/Http/Controllers/Apis/CodonController.php
@@ -12,15 +12,7 @@ class CodonController extends Controller
 {
     public function responseAll()
     {
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-        echo $ip;
-        // $res = CodonsServices::getInstance()->responseAll();
-        // return $res;
+        $res = CodonsServices::getInstance()->responseAll();
+        return $res;
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT'],
 
-    'allowed_origins' => ['http://159.203.14.63', 'http://localhost:3000', 'http://159.203.14.63:80', 'http://localhost:8000', 'http://stage.cbbio.online:80', 'https://legendary-blancmange-fc233d.netlify.app'],
+    'allowed_origins' => ['http://159.203.14.63', 'http://localhost:3000', 'http://159.203.14.63:80', 'http://localhost:8000', 'http://stage.cbbio.online:80', 'https://stage.cbbio.online'],
 
     'allowed_origins_patterns' => ['Google'],
 
